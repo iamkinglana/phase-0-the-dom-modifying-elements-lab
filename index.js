2a79a2159caaf5ae77a441b535956c2461a68bf7
+ <+>
2023-03-13 00:39:46 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+
+const dele = document.getElementById('main')
+dele.remove();
+
+//new header var
+let newHeader = document.createElement("h1")
+
+//newheader id
+newHeader.id = "victory";
+
+
+newHeader.textContent = "Enock is the champion";
+
+
